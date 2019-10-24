c0f11a8f5bdf461a2a317ccfaddebf91ac87df8e
pouyapanahandeh <panahandehpooya@gmail.com>
2019-10-24 09:49:25 +0200

    cleaning the page and structure for the API

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Fide</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
       >
         <span class="mr-2">Latest Release</span>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div>
+    <v-alert
+      v-model="alert"
+      dismissible
+      color="cyan"
+      border="left"
+      elevation="2"
+      colored-border
+      icon="mdi-twitter"
+    >
+      You've got <strong>5</strong> new updates on your timeline!.
+    </v-alert>
   </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        alert: true,
+      }
+    },
+  }
+</script>
