e2e398788677f504dfb657f2a8cf8f152be62d0d
pouyapanahandeh <panahandehpooya@gmail.com>
2019-11-12 17:35:27 +0100

    NavBar and SectionOne for the landing page

---
+++ src/components/landingPage/sectionOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-container fluid>
+            <v-row no-gutters>
+                <v-col>
+
+                </v-col>
+                <v-col>
+                    
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-container>
+</template>
---
+++ src/components/layout/HeaderNav.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <v-container fluid>
+            <v-toolbar id="navbar">
+                <v-toolbar-title>
+                    Fide
+                </v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+                <v-toolbar-items>
+                    <v-btn class="ma-2" text>Features</v-btn>
+                    <v-btn class="ma-2" text>Privacy & Safety</v-btn>
+                    <v-btn class="ma-2" text>Track Rides</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+        </v-container>
+    </v-container>
+</template>
+<style>
+#navbar{
+    border-width: 0px;
+    -webkit-box-shadow: 0px 0px;
+    box-shadow: 0px 0px;
+    background-color: rgba(0,0,0,0.0);
+    background-image: -webkit-gradient(linear, 50.00% 0.00%, 50.00% 100.00%, color-stop( 0% , rgba(0,0,0,0.00)),color-stop( 100% , rgba(0,0,0,0.00)));
+    background-image: -webkit-linear-gradient(270deg,rgba(0,0,0,0.00) 0%,rgba(0,0,0,0.00) 100%);
+    background-image: linear-gradient(180deg,rgba(0,0,0,0.00) 0%,rgba(0,0,0,0.00) 100%);
+}
+</style>
