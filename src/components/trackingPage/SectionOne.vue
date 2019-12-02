52875c8485ce8dbd913d14afda31c61cf7f3dbe2
pouyapanahandeh <panahandehpooya@gmail.com>
2019-12-02 09:27:08 +0100

    tracking page

---
+++ src/components/trackingPage/SectionOne.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+      <v-container fluid>
+          <v-row align="center" justify="center">
+            <v-alert text prominent dense dismissible border="left" type="error" icon="mdi-cloud-alert">
+                To use Fide in this version, you need to be added as a tester. please use fide support.
+            </v-alert>
+          </v-row>
+          <v-row justify="center" align="center">
+              <v-col cols="10" offset="1">
+                  
+              </v-col>
+          </v-row>
+      </v-container>
+  </v-container>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        e1: 0,
+      }
+    },
+  }
+</script>
+<style>
+#trackingCard{
+    border-radius: 10px;
+}
+</style>
--- src/views/Track.vue
+++ src/views/Track.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    <HeaderNav />
+    <v-content>
+      <SectionOne />
+    </v-content>
+    <FooterNav />
+  </v-app>
 </template>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>
+import HeaderNav from '@/components/layout/HeaderNav';
+import SectionOne from '@/components/trackingPage/SectionOne'
+import FooterNav from '@/components/layout/FooterNav';
+export default {
+  name: 'App',
+
+  components: {HeaderNav, SectionOne, FooterNav},
+
+  data: () => ({  
+    
+  }),
+};
+</script>
